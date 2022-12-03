daed553dc349a683d67e0b1e3fc769dd7de123c0
skylerpfli <skylerpfli@tencent.com>
2022-12-03 23:50:50 +0800

    本地接入Conch

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_conch_plugin/annotation/conch_scope.dart';
+import 'package:flutter_conch_plugin/conch_dispatch.dart';
 import 'package:gsy_github_app_flutter/app.dart';
 import 'package:gsy_github_app_flutter/env/config_wrapper.dart';
 import 'package:gsy_github_app_flutter/env/env_config.dart';
@@ -9,18 +12,35 @@ import 'package:gsy_github_app_flutter/page/error_page.dart';
 
 import 'env/dev.dart';
 
-void main() {
+bool useConch = true;
+
+@ConchScope()
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (useConch) {
+    var source = await rootBundle.loadString('static/conch_data/conch_result.json');
+    ConchDispatch.instance.loadSource(source);
+    ConchDispatch.instance.callStaticFun(library: 'package:gsy_github_app_flutter/main.dart', funcName: 'mainInner');
+    return;
+  }
+
+  mainInner();
+}
+
+mainInner() {
   runZonedGuarded(() {
     ErrorWidget.builder = (FlutterErrorDetails details) {
       Zone.current.handleUncaughtError(details.exception, details.stack!);
+
       ///此处仅为展示，正规的实现方式参考 _defaultErrorWidgetBuilder 通过自定义 RenderErrorBox 实现
-      return ErrorPage(
-          details.exception.toString() + "\n " + details.stack.toString(), details);
+      return ErrorPage(details.exception.toString() + "\n " + details.stack.toString(), details);
     };
     runApp(ConfigWrapper(
       child: FlutterReduxApp(),
       config: EnvConfig.fromJson(config),
     ));
+
     ///屏幕刷新率和显示率不一致时的优化，必须挪动到 runApp 之后
     GestureBinding.instance.resamplingEnabled = true;
   }, (Object obj, StackTrace stack) {
---
+++ transform/lib/flutter_frontend_server/starter.dart
@@ -0,0 +1,18 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart=2.8
+library frontend_server;
+
+import 'dart:io';
+
+import 'package:conch_build/flutter_frontend_server/server.dart';
+
+/// 动态指向
+void main(List<String> args) async {
+  final int exitCode = await starter(args);
+  if (exitCode != 0) {
+    exit(exitCode);
+  }
+}
