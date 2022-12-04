3bb041ded4f33f35a4dafa085cd5f57484c6e37a
skylerpfli <skylerpfli@tencent.com>
2022-12-04 16:08:17 +0800

    规避isolate执行

--- lib/common/utils/code_utils.dart
+++ lib/common/utils/code_utils.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
+import 'package:flutter_conch_plugin/annotation/conch_exclude.dart';
 
 ///isolate 的 compute 需要静态方法
+@ConchExclude()
 class CodeUtils {
-  static List<dynamic> decodeListResult(String? data) {
-    return json.decode(data!);
+  static List<dynamic> decodeListResult(String data) {
+    return json.decode(data);
   }
 
-  static Map<String, dynamic> decodeMapResult(String? data) {
-    return json.decode(data!);
+  static Map<String, dynamic> decodeMapResult(String data) {
+    return json.decode(data);
   }
 
   static String encodeToString(String data) {
